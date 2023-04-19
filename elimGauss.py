eeee645b3a16d7477fe1f1f95d9c17255c569214
Bruno Augusto Furquim <97642457+BrunoAFurquim@users.noreply.github.com>
2023-04-19 15:33:50 -0300

    Create eliminGauss_pivot()
    método de eliminação de Gauss, porém, realiza um pivotamento parcial

--- elimGauss.py
+++ elimGauss.py
@@ -22,15 +22,46 @@ def eliminGauss(A, b):
     
     return x
 
+# metodo Gauss Pivotamente parcial
+import numpy as np
+
+def eliminGauss_pivot(A, b):
+    n = len(b)
+    A = A.astype(float)
+    b = b.astype(float)
+    # Etapa de escalonamento
+    for k in range(n-1):  # Loop sobre as colunas
+        # Escolha do pivô com pivotamento parcial
+        p = np.argmax(np.abs(A[k:, k])) + k  # Índice da linha com maior valor absoluto
+        # Permutação das linhas k e p
+        if p != k:
+            A[[k, p]] = A[[p, k]]
+            b[[k, p]] = b[[p, k]]
+        for i in range(k+1, n):  # Loop sobre as linhas abaixo da diagonal
+            # Cálculo do multiplicador
+            m = A[i,k] / A[k,k]
+            # Operações de eliminação
+            A[i,k+1:n] -= m * A[k,k+1:n]
+            b[i] -= m * b[k]
+    
+    # Etapa de substituição retroativa
+    x = np.zeros(n)
+    x[n-1] = b[n-1] / A[n-1,n-1]  # Última variável desconhecida
+    for i in range(n-2, -1, -1):  # Loop reverso sobre as linhas
+        # Cálculo das variáveis desconhecidas restantes
+        x[i] = (b[i] - np.dot(A[i,i+1:n], x[i+1:n])) / A[i,i]
+    
+    return x
 
 A = np.array([[3, 3, 1],
               [2, 2, -1],
               [1, -1, 5]])
 
-b = np.array([1, 1, 1])
+b = np.array([7, 3, 5])
 
-x = eliminGauss(A, b)
+x = eliminGauss_pivot(A, b)
 
 print("Solução:")
 for i, xi in enumerate(x):
     print("x{} = {:.2f}".format(i+1, xi))
+    
