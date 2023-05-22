9bca48e28ead8284bba0b3e0e7cbd673b27db818
Gabriel Bevilacqua Barros <92558878+GabrielBBarros@users.noreply.github.com>
2023-05-21 21:41:30 -0300

    Update interpolacao.py

--- interpolacao/interpolacao.py
+++ interpolacao/interpolacao.py
@@ -1,5 +1,29 @@
-import numpy as np
+def lagrange_interpolation(x, y, xi):
+    n = len(x)
+    yi = 0.0
+    
+    for i in range(n):
+        Li = 1.0
+        for j in range(n):
+            if i != j:
+                Li *= (xi - x[j]) / (x[i] - x[j])
 
-p = [1, -6, 8]
-x = [-1, 0, 3]
-y = [15, 8, -1]
+
+        yi += y[i] * Li
+
+        print(f'L{i}(x) = {Li}')
+    
+    return yi
+
+
+# Exemplo de pontos conhecidos
+x = [1, 2, 3]
+y = [1, 4, 9]
+
+# Valor a ser interpolado
+xi = 2.5
+
+# Realiza a interpolação
+result = lagrange_interpolation(x, y, xi)
+
+print(f'\nO valor interpolado em xi = {xi} é yi = {result}')
