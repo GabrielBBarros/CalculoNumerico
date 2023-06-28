4d515dbc114356b63c80ab21673c9f187379fed5
Bruno Augusto Furquim <97642457+BrunoAFurquim@users.noreply.github.com>
2023-06-28 11:42:02 -0300

    feat: dedução por série de taylor

---
+++ p2/diferenciacao numerica/centrada.py
@@ -0,0 +1,4 @@
+# expandimos a série de taylor até a terceira ordem e fazemos a diferença entre f(x+h) e f(x-h)
+# f(x + h) - f(x-h) = 2hf'(x) + h³/6(f'''(E1) + f'''(E2)) onde E1 está entre x e x+h e E2 está entre x e x-h
+# pelo temorema do ponto médio podemos assumir que existe um E3 entre E1 e E2
+#isolando f'(x) e descartando o erro, obtemos f'(x) = (f(x + h) - f(x - h))/2h = De(f)
