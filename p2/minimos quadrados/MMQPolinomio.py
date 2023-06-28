6ac050166ebcb21ca4c4efb4072c2d57fe704240
Bruno Augusto Furquim <97642457+BrunoAFurquim@users.noreply.github.com>
2023-06-28 11:42:16 -0300

    feat: MMQ methods

---
+++ p2/minimos quadrados/MMQPolinomio.py
@@ -0,0 +1,53 @@
+# O método dos mínimos quadrados é uma técnica usada para encontrar a melhor aproximação de um conjunto de pontos por uma função matemática. Ele é comumente aplicado em problemas de regressão, onde o objetivo é encontrar a equação de uma curva que melhor se ajusta aos dados observados.
+# minimizar o erro |a1x + a0 - y| para um conjunto de pontos (Xi, Yi), i=0,..., m
+# para isso determinamos a função φ(a) = ||Aa - Y||² onde:
+# A = |1 x0|    ,   a = |a0|  ,   Y = | y0|
+#     |1 x1|        a = |a1|          | y1|
+#     |... |                          |...|
+#     |1 xm|                          | ym|
+# e a norma || ||² é definida por: ||X||² = √(Σ(xi²)) para um vetor X = {Xi}
+# desta forma temos:
+# φ(a) = 1/2 * √(Σ(a1xi + a0 - yi)²)² => 1/2 * Σ(a1xi + a0 - yi)²
+# para minimizar φ(a) devemos ter ∂φ(a)/∂a = 0 e ∂φ(a)/∂b = 0
+# ∂φ(a)/∂a = 0 = Σ(a1xi + a0 - yi)
+# ∂φ(a)/∂b = 0 = Σ(xi(a1xi² + a0xi - yixi))
+# Logo, podemos construir o seguite sistema equivalente:
+# AtAa = AtAy => |m+1     Σ(xi)| * |a0| = | Σ(yi) |
+#                |Σ(xi)  Σ(xi²)|   |a1|   |Σ(yixi)|
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Pontos tabelados (xi, yi)
+x = np.array([0, 1, 2, 3, 4, 5])
+y = np.array([-1, 0, 3, 8, 15, 24])
+
+# Construir as matrizes X e Y
+X = np.column_stack((np.ones_like(x), x**3))
+Y = y.reshape(-1, 1)
+
+# Resolver o sistema de equações lineares
+coef = np.linalg.lstsq(X, Y, rcond=None)[0]
+
+# Obter os coeficientes a e b
+a, b = coef.flatten()
+
+# Construir o polinômio P(x)
+def P(x):
+    return a + b * x**3
+
+# Gerar pontos para plotagem
+x_plot = np.linspace(min(x), max(x), 100)
+y_plot = P(x_plot)
+
+# Imprimir os resultados
+print("Coeficientes a e b:", a, b)
+print("P(x) = ",a+b)
+
+# Plotar o gráfico
+plt.scatter(x, y, color='blue', label='Pontos Tabelados')
+plt.plot(x_plot, y_plot, color='red', label='Polinômio Aproximado')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid(True)
+plt.show()
---
+++ p2/minimos quadrados/MMQlinha.py
@@ -0,0 +1,49 @@
+#A ideia básica por trás do método é encontrar uma reta que minimize a distância vertical entre os pontos de dados e a reta. Isso é feito ajustando os coeficientes da equação da reta (y = ax + b) de forma a minimizar a soma dos quadrados das diferenças entre os valores observados de y e os valores preditos pela reta ajustada.
+
+#Vamos analisar o código passo a passo:
+
+ #   A função MMQ recebe dois arrays, x e y, que representam os pontos de dados que queremos ajustar.
+
+ #   Em seguida, a função calcula algumas somas e produtos necessários para obter os coeficientes a e b da equação da reta.
+#        -n representa o número de pontos de dados.
+#        -sum_x é a soma dos valores de x.
+#        -sum_y é a soma dos valores de y.
+#        -sum_x_squared é a soma dos quadrados dos valores de x.
+#        -sum_xy é a soma dos produtos dos valores de x e y.
+
+#    Os coeficientes a e b são calculados usando as fórmulas dos mínimos quadrados:
+#        a = (n * sum_xy - sum_x * sum_y) / (n * sum_x_squared - sum_x**2)
+#        b = (sum_y - a * sum_x) / n
+
+#    Por fim, a função retorna os coeficientes a e b.
+
+#No exemplo fornecido, os dados de exemplo são os pontos (1, 2), (2, 3), (3, 4), (4, 5), e (5, 6).
+#O código calcula e imprime os coeficientes a e b da equação da reta que melhor se ajusta a esses pontos.
+#Esses coeficientes podem ser interpretados como o coeficiente angular e o coeficiente linear da reta, respectivamente.
+
+
+import numpy as np
+
+def MMQ(x, y):
+    # Calcula os coeficientes a e b da equação da reta y = ax + b
+    n = len(x)
+    sum_x = np.sum(x)
+    sum_y = np.sum(y)
+    sum_x_squared = np.sum(x**2)
+    sum_xy = np.sum(x*y)
+
+    a = (n * sum_xy - sum_x * sum_y) / (n * sum_x_squared - sum_x**2)
+    b = (sum_y - a * sum_x) / n
+
+    return a, b
+
+# Dados de exemplo
+x = np.array([-2,-1,0,1,2])
+y = np.array([0,0,-1,0,7])
+
+# Calcula os coeficientes a e b usando o método dos mínimos quadrados
+a, b = MMQ(x, y)
+
+# Imprime os coeficientes
+print("Coeficiente a:", a)
+print("Coeficiente b:", b)
